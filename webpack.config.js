4bbd7e7a271ca7405d061bd0d7c14c570c016d89
dmitriypavlovskyi <pavlovskyidmitriy@gmail.com>
2018-09-10 13:01:08 +0300

    Basic webpack with babel added

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	entry: './script.js',
+	output: {
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+			  test: /\.js?/, 
+			  loader: 'babel-loader', 
+			  exclude: /node_modules/
+			}
+		]
+	}
+};
